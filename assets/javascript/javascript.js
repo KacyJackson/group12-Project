0de305aca187ef8ea0aa730bcc7736ab1a5485c9
Gerald Perriman <geraldperriman@lawn-128-61-41-61.lawn.gatech.edu>
2017-06-17 13:59:40 -0400

    javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,30 +9,61 @@ btn.addEventListener("click", function(){
 var authKey = "818356de4c7f4ac49a7f1382bc69cfda";
 
 //  variables to hold results
-var Name = "";
-var Email Address = "";
+var Name = " John Doe";
+var Phone = "770-123-4567";
+var Email = "example@email.com";
+var city = "Atlanta";
+var countryCode = "US";
+var stateCode =" GA";
+var postalCode = "30312";
+var primaryAd = "123 Main St."
+var primaryAd2 = "Apt 1";
 
-var queryURLBase = "";
+   var queryURL = "https://proapi.whitepages.com/3.2/identity_check?api_key=818356de4c7f4ac49a7f1382bc69cfda&email_address="+ Email+"&primary.address.city="+ city+"&primary.address.country_code="+ countryCode+"&primary.address.postal_code="+ postalCode+"&primary.address.state_code="+ stateCode+"&primary.address.street_line_1="+ primaryAd+"&primary.address.street_line_2="+ primaryAd2+"&primary.name="+ Name+"&primary.phone="+ Phone+"";
 
-// functions
+      // Perfoming an AJAX GET request to our queryURL
+     
+	$.ajax({
+		url: queryURL, 
+		method: "GET"
+	}).done(function (response){
+	console.log(response);
+	});
 
-// parameters
-
-function runQuery(,queryURL){
-
-//AJAX function
-
-$.ajax({
-	url:queryURL,
-	method: "GET"
-}).done(function(response){
-
+	console.log(Name);
+	console.log(Phone);
+	console.log(Email);
+	console.log(city);
+	console.log(countryCode);
+	console.log(stateCode);
+	console.log(postalCode);
+	console.log(primaryAd);
+	console.log(primaryAd2);
 
 
 
+        
+
+     
+
+          
 
 
-})
 
 
-}
+	
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
