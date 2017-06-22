9cde14ae1e9cbc89c4bf6be4b87b8aeae3a341b8
Gerald Perriman <geraldperriman@lawn-128-61-41-61.lawn.gatech.edu>
2017-06-22 18:50:05 -0400

    new js

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,7 +6,7 @@ btn.addEventListener("click", function(){
 });
 
 // authentication key
-var authKey = "";
+var authKey = "nGUpOblL6MmXFPSZHiHf6B6F4qTcAbFsiNWCWUy3mFjHxVjmB0kW2DGbb5KHIe1y";
 
 //  variables to hold results
 var Name = "";
--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,41 +5,85 @@ btn.addEventListener("click", function(){
 
 });
 
-// authentication key
-var authKey = "818356de4c7f4ac49a7f1382bc69cfda";
 
-//  variables to hold results
-var Name = " John Doe";
-var Phone = "770-123-4567";
-var Email = "example@email.com";
-var city = "Atlanta";
-var countryCode = "US";
-var stateCode =" GA";
-var postalCode = "30312";
-var primaryAd = "123 Main St."
-var primaryAd2 = "Apt 1";
 
-   var queryURL = "https://proapi.whitepages.com/3.2/identity_check?api_key=818356de4c7f4ac49a7f1382bc69cfda&email_address="+ Email+"&primary.address.city="+ city+"&primary.address.country_code="+ countryCode+"&primary.address.postal_code="+ postalCode+"&primary.address.state_code="+ stateCode+"&primary.address.street_line_1="+ primaryAd+"&primary.address.street_line_2="+ primaryAd2+"&primary.name="+ Name+"&primary.phone="+ Phone+"";
+// //  variables to hold results
+// var phoneInput = document.getElementById('Phone');
+// var Phone = phoneInput.value;
 
-      // Perfoming an AJAX GET request to our queryURL
+
+var Phone = ""; 
+
+
+
+
+   // var queryURL = "https://proapi.whitepages.com/3.2/identity_check?api_key=818356de4c7f4ac49a7f1382bc69cfda&email_address="+ Email+"&primary.address.city="+ city+"&primary.address.country_code="+ countryCode+"&primary.address.postal_code="+ postalCode+"&primary.address.state_code="+ stateCode+"&primary.address.street_line_1="+ primaryAd+"&primary.address.street_line_2="+ primaryAd2+"&primary.name="+ Name+"&primary.phone="+ Phone+"";
+        // Perfoming an AJAX GET request to our queryURL
      
-	$.ajax({
-		url: queryURL, 
-		method: "GET"
-	}).done(function (response){
-	console.log(response);
-	});
+ 
 
-	console.log(Name);
+	 $("#Phone").val(Phone);
+
+	  $("#btn").on("click", function(event) {
+  // This line allows us to take advantage of the HTML "submit" property
+  // This way we can hit enter on the keyboard and it registers the search
+  // (in addition to clicks).
+  event.preventDefault();
+
+	// Grabbing text the user typed into the search input
+  	 var input = $("#Phone").val().trim();
+  	 var queryURL ="https://proapi.whitepages.com/3.0/phone.json?api_key=bbefe26042d944dbbf213f36a76be0e0&phone="+ input;
+  	 $.ajax({
+
+  // The 'type' property sets the HTTP method.
+  // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+  type: 'GET',
+
+ 
+  url: queryURL,
+
+  
+  contentType: 'text/plain',
+
+  xhrFields: {
+    
+    withCredentials: false
+  },
+
+  headers: {
+    
+  },
+
+  success: function(response) {
+   console.log(response);
+  },
+
+  error: function() {
+  }
+});
+ 
+
+
+  	
+
+});
+
+
+
+ 
+
+ 
+
+
+
+
+ 
+
+
+
+
 	console.log(Phone);
-	console.log(Email);
-	console.log(city);
-	console.log(countryCode);
-	console.log(stateCode);
-	console.log(postalCode);
-	console.log(primaryAd);
-	console.log(primaryAd2);
-
+	
 
 
         
