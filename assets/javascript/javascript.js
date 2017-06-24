44d8e86cb94b91bf55a041eed00d21fa41896d50
Menelik Tefera <menelikworku@gmail.com>
2017-06-24 11:23:59 -0400

    almost there

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,7 +11,9 @@
 
 var btn = document.getElementById("btn");
 var database = firebase.database();
-
+var firstFolder = "";
+var name = "";
+var address = "";
 
 
 // //  variables to hold results
@@ -19,10 +21,7 @@ var database = firebase.database();
 // var Phone = phoneInput.value;
 
 
-var Phone = ""; 
 
-
-$("#Phone").val(Phone);
 
 $("#btn").on("click", function(event) {
   event.preventDefault();
@@ -30,31 +29,95 @@ $("#btn").on("click", function(event) {
 	// Grabbing text the user typed into the search input
 	var input = $("#Phone").val().trim();
   $("#Phone").val("");
-	var queryURL ="https://proapi.whitepages.com/3.0/phone.json?api_key=bbefe26042d944dbbf213f36a76be0e0&phone="+ input;
+	var queryURL ="https://proapi.whitepages.com/3.0/phone.json?api_key=dd6cf1d08eeb47038e757269a297655e&phone="+ input;
 
 	$.ajax({
     url: queryURL,
     method: "GET"
   })
   .done(function(response) {
-    console.log(response);
     database.ref().set({
       result: response
     })
-  });   	
+  });
+
+  $('#form').hide();
+
+  var nameRef = database.ref('result/belongs_to/0');
+  nameRef.on('value', function(data){
+    var name = "<strong>Name</strong>: " + data.val().name;
+    var gender = "<strong>Gender</strong>: " + data.val().gender;
+    var age = "<strong>Age Range</strong>: " + data.val().age_range;
+
+    var display = $('<div>');
+    display.attr('id', 'display');
+    display.html("<strong><span style='color:#2F6FB9'>IDENTITY</span></strong><br>" + name + "<br>" + gender + "<br>" + age);
+    $('#result').html(display);
+    $('#check').html("Results");
+  },function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+  var addressRef = database.ref('result/current_addresses/0');
+  addressRef.on('value', function(data){
+    var address = "<br><strong><span style='color:#2F6FB9'>KNOWN ADDRESSES</span></strong><br><strong>Address 1</strong>: " + data.val().street_line_1 + ", " + data.val().city + " " + data.val().state_code + " " + data.val().postal_code;
+    var display = $('<div>');
+    display.attr('id', 'display');
+    display.append(address);
+    $('#result').append(display);
+  },function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+  });
+
+  var addressRef = database.ref('result/current_addresses/1');
+  addressRef.on('value', function(data){
+    var address = "<strong>Address 2</strong>: " + data.val().street_line_1 + ", " + data.val().city + " " + data.val().state_code + " " + data.val().postal_code;
+    var display = $('<div>');
+    display.attr('id', 'display');
+    display.append(address);
+    $('#result').append(display);
+  },function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+  });
+
+  $('.btn-default').show()
 
 });
 
-var latRef = database.ref('result/current_addresses/0/lat_long/latitude');
-var longRef = database.ref('result/current_addresses/0/lat_long/longitude');
-latRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
-longRef.on('value', function(snapshot) {
-console.log(snapshot.val());
-});
-});
- 
+function initMap() {
+    var lat_longRef = database.ref('result/current_addresses/0/lat_long');
+      lat_longRef.on('value', function(data){
+      var lati = data.val().latitude;
+      console.log(lati);
+      var long = data.val().longitude;
+      
+      var uluru = {lat: lati, lng: long};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: uluru
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      })
+    })
+  }
 
+  initMap();
+  //var newDiv = $('<div>');
+  //newDiv.attr('id', 'map');
+  //$('.panel-body').append(newDiv);
+
+
+   
+
+
+
+//var lat_longRef = database.ref('result/current_addresses/0/lat_long');
+//lat_longRef.on('value', function(data){
+  //console.log("latitude: " + data.val().latitude);
+  //console.log("longitude: " + data.val().longitude);
+//})
  
 
 
